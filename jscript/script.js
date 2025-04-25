ce20f74f2c630e750b24ca4787dcf80cea75e073
RealSniperZ <realsniperz09@gmail.com>
2025-04-25 08:00:41 +0200

    hasta aqui jsjs

---
+++ jscript/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    VANTA.BIRDS({
+        el: "#vanta-bg",
+        mouseControls: true,
+        touchControls: true,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundColor: 0x0,
+        color1: 0xffffff,
+        color2: 0x111111,
+        cohesion: 190.00,
+        quantity: 5.0
+    });
+});
